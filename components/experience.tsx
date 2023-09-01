177f7d1232e6e8377debf405b86aed0d95806522
Evan Leong <evleong1@gmail.com>
2023-09-01 13:00:43 -0700

    added styles

--- components/experience.tsx
+++ components/experience.tsx
@@ -13,6 +13,7 @@ import exp from "constants";
 import { useTheme } from "@/context/theme-context";
 
 
+//experience section
 export default function Experience() {
 
     const { ref } = useSectionInView("Experience", 0.5);
